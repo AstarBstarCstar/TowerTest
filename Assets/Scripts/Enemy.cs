2705675f7dd95776f579e662121f569f64699790
MoonSangDon <moon3225@naver.com>
2022-05-08 22:46:28 +0900

    누락된에셋 수정

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+	private Transform target;
+	private int wavepointIndex = 0;
+
+	private void Start ()
+	{
+		target = WaveManager.instance.points[0];
+	}
+
+	private void Update ()
+	{
+        Move();
+	}
+
+    private void Move()
+    {
+        Vector3 dir = target.position - transform.position;
+		transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+		{
+			GetNextWaypoint();
+		}
+    }
+
+	private void GetNextWaypoint()
+	{
+		if (wavepointIndex >= WaveManager.instance.points.Length - 1)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		wavepointIndex++;
+		target = WaveManager.instance.points[wavepointIndex];
+	}
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager instance { get; private set; }
+
+    public GameObject waypoints;
+    public Transform[] points;
+
+    public Enemy enemyPrefab;
+	public Transform spawnPoint;
+
+	public float timeBetweenWaves = 5f;
+	private float countdown = 2f;
+
+	private int waveIndex = 0;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    private void Start() {
+        SetWayPoints();
+    }
+
+	private void Update ()
+	{
+		Spawn();
+	}
+
+    private void Spawn()
+    {
+        if (countdown <= 0f)
+		{
+			StartCoroutine(SpawnWave());
+			countdown = timeBetweenWaves;
+		}
+
+		countdown -= Time.deltaTime;
+    }
+
+	IEnumerator SpawnWave ()
+	{
+		waveIndex++;
+
+		for (int i = 0; i < waveIndex; i++)
+		{
+			SpawnEnemy();
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+
+	private void SpawnEnemy ()
+	{
+		Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+
+    private void SetWayPoints()
+    {
+        points = new Transform[waypoints.transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = waypoints.transform.GetChild(i);
+        }
+    }
+}
